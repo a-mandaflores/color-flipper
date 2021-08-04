cbf3ee2e0d8069eaceb9999da9261ac8a1d27292
Amanda <nanda.hamandadesouza@gmail.com>
2021-08-04 16:17:04 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "#F1f4f8"];
+const colors = ["green", "red", "blue"];
 
 const btn = document.getElementById("btn")
 const color = document.querySelector(".color")
